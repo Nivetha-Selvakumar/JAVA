d25a154b1b7c40e617f403eaa1af276493417164
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2023-11-14 10:55:56 +0530

    thirdcommit

---
+++ Day 5/Method3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.*;
+import java.lang.String;
+
+public class Method3 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String a=sc.nextLine();
+        String b= sc.nextLine();
+        System.out.println(a.toLowerCase());
+        System.out.println(b.toUpperCase());
+        sc.close();
+    }
+
+}
