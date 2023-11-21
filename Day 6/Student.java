d49f67ed0ce9455146062a87c4c9b36b68459896
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2023-11-21 12:03:57 +0530

    Day6

---
+++ Day 6/Student.java
@@ -0,0 +1,10 @@
+package pack;
+public class Student{
+    private String name;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+}
---
+++ Day 6/Test.java
@@ -0,0 +1,8 @@
+package pack;
+public class Test {
+    public static void main(String[] args){
+        pack.Student s=new pack.Student();
+        s.setName("NIVE");
+        System.out.println(s.getName());
+    }
+}
