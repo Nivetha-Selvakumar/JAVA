12272cab8c64e01e9a6e14814f4297f7ced73c8a
Nivetha-Selvakumar <nivethaselvakumar23@gmail.com>
2023-11-21 12:27:34 +0530

    fourthcommit

---
+++ Day 6/EncapTest.java
@@ -0,0 +1,9 @@
+package numberTest;
+public class EncapTest {
+    public static void main(String[] args){
+        numberTest.Number s=new numberTest.Number();
+        s.set(23);
+        System.out.println(s.get());
+    }
+}
+
---
+++ Day 6/Number.java
@@ -0,0 +1,10 @@
+package numberTest;
+public class Number{
+    private int num;
+    public int get(){
+        return num*num;
+    }
+    public void set(int num){
+        this.num=num;
+    }
+}
